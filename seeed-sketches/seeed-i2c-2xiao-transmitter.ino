57b913b8dde9623fff33322423c42afee92573ac
Jeremy Ellis <keyfreemusic@gmail.com>
2026-07-26 15:21:16 -0700

    Add files via upload

---
+++ seeed-sketches/seeed-i2c-2xiao-transmitter.ino
@@ -0,0 +1,51 @@
+
+/*
+ *  
+ *  seeed-i2c-2xiao-transmitter.ino
+ *  
+ *   Instructions: 
+
+ *   Connect Arduino 1 receiver GND to Arduino 2 transmitter GND
+ *   Connect Arduino 1 receiver 3V3 to Arduino 2 transmitter 3V3 
+ *   Connect Arduino 1 receiver SDA to Arduino 2 transmitter SDA  (on XIAO SDA D4)
+ *   Connect Arduino 1 receiver SCL to Arduino 2 transmitter SCL  (on XIAO SCL D5)
+ *   Upload seeed-i2c-2xiao-receiver.ino to Arduino 1 
+ *   Upload seeed-i2c-2xiao-transmitter.ino to Arduino 2 
+ *   Watch Serial Monitor on Arduino 1 receiver
+
+ * Note: If one of the I2C modules uses 5V  then you would need resistors to protect the XIAO
+ * research it you will need resistors to protect the I2C pins 
+
+
+ * 
+*/
+
+
+#include <Wire.h> 
+
+#define receiverAddress 9 
+
+uint8_t counter = 0; 
+
+void setup() { 
+  Serial.begin(115200); 
+  Wire.begin(); 
+} 
+
+ 
+
+void loop() { 
+  counter++; 
+  if (counter > 255) { 
+    counter = 0; 
+  } 
+
+  Wire.beginTransmission(receiverAddress); 
+  Wire.write(counter); 
+  Wire.endTransmission(); 
+  Serial.println("Sent "+String(counter)); 
+  delay(500); 
+
+} 
+
+ 
